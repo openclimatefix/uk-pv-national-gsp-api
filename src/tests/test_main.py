e37d9f59389594611712ebcd54e5aa2566cac35b
peterdudfield <peter.dudfield@hotmail.com>
2021-10-01 12:41:52 +0100

    flake8

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json()['version'] == version
+    assert response.json()["version"] == version
 
 
 def test_read_latest():
@@ -19,6 +19,7 @@ def test_read_latest():
     r = MultipleGSP(**response.json())
     assert len(r.gsps) == 339
 
+
 def test_floor_30_minutes():
     """
     Test if floor_30_minutes_dt method works by testing against every minute in a hour
@@ -26,11 +27,11 @@ def test_floor_30_minutes():
     For minutes in range [30, 60) => Will floor to 30 minutes
     """
     list_of_time = get_every_minute()
-    input = datetime.fromisoformat('2021-09-30 12:15:05')
-    expected_output = datetime.fromisoformat('2021-09-30 12:00:00')
+    input = datetime.fromisoformat("2021-09-30 12:15:05")
+    expected_output = datetime.fromisoformat("2021-09-30 12:00:00")
 
-    assert floor_30_minutes_dt(input) == expected_output # Basic control test
-    
+    assert floor_30_minutes_dt(input) == expected_output  # Basic control test
+
     for time in list_of_time:
         floor_minute = floor_30_minutes_dt(time)
         if time.minute < 30:
