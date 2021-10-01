47d13b55e0682fffa3299731c4d4b048dd1f03ed
peterdudfield <peter.dudfield@hotmail.com>
2021-10-01 12:43:28 +0100

    tidy

--- script/make_openjson.api.py
+++ script/make_openjson.api.py
@@ -9,12 +9,9 @@ import requests
 import json
 
 # get schema
-r = requests.get('http://127.0.0.1:8000/openapi.json')
+r = requests.get("http://127.0.0.1:8000/openapi.json")
 d = r.json()
 
 # save to file
-with open('docs.json', 'w') as f:
+with open("docs.json", "w") as f:
     json.dump(d, f, indent=4)
-
-
-
