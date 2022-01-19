68a0d17066189772dfc945fa85ccfb0048db3116
peterdudfield <peter.dudfield@hotmail.com>
2022-01-19 20:27:18 +0000

    0.1.6

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ from dummy import create_dummy_national_forecast
 
 logger = logging.getLogger(__name__)
 
-version = "0.1.5"
+version = "0.1.6"
 description = """
 The Nowcasting API is still under development. It only returns zeros for now.
 """
