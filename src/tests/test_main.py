e44b57096ceb7d83eff3c8fbb3ada5a52d9993c1
peterdudfield <peter.dudfield@hotmail.com>
2022-01-19 17:30:48 +0000

    run tests in same sql session

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -2,6 +2,7 @@
 
 from fastapi.testclient import TestClient
 from nowcasting_forecast.database.models import Forecast, ManyForecasts
+from nowcasting_forecast.database.fake import make_fake_forecasts
 
 from main import app, get_session, version
 
@@ -15,8 +16,11 @@ def test_read_main():
     assert response.json()["version"] == version
 
 
-def test_read_latest_one_gsp(db_session, forecasts):
+def test_read_latest_one_gsp(db_session):
     """Check main GB/pv/gsp/{gsp_id} route works"""
+
+    forecasts = make_fake_forecasts(gsp_ids=list(range(0,10)))
+    db_session.add_all(forecasts)
 
     app.dependency_overrides[get_session] = lambda: db_session
 
@@ -26,8 +30,11 @@ def test_read_latest_one_gsp(db_session, forecasts):
     _ = Forecast(**response.json())
 
 
-def test_read_latest_all_gsp(db_session, forecasts):
+def test_read_latest_all_gsp(db_session):
     """Check main GB/pv/gsp route works"""
+
+    forecasts = make_fake_forecasts(gsp_ids=list(range(0,338)))
+    db_session.add_all(forecasts)
 
     app.dependency_overrides[get_session] = lambda: db_session
 
