7447d1f39ef684ea9e320837eafefcbd6e8805c7
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2022-02-02 11:59:42 +0000

    Update src/gsp.py
    Co-authored-by: Flo <6052785+flowirtz@users.noreply.github.com>

--- src/gsp.py
+++ src/gsp.py
@@ -4,7 +4,7 @@ from nowcasting_dataset.data_sources.gsp.eso import get_gsp_shape_from_eso
 
 
 def get_latitude_longitude_gsp_boundaries_from_eso() -> gpd.GeoDataFrame:
-    """Get GSP boundaries"""
+    """Get GSP boundaries in lat/lon format (EPSG:4326)"""
 
     # get gsp boundaries
     boundaries = get_gsp_shape_from_eso()
