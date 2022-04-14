d85f83c54c1e5229258fcb2c5a18c84459f7179c
peterdudfield <peter.dudfield@hotmail.com>
2022-04-14 09:44:54 +0100

    comment out pv

--- src/pv.py
+++ src/pv.py
@@ -17,26 +17,26 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.get("/pv_latest", response_model=List[PVYield])
-def get_latest_pv_data(session: Session = Depends(get_session_pv)) -> List[PVYield]:
-    """Get Latest PV data from specific pv sites
-
-    Only provide PV data received within the last 1 hour
-    """
-
-    # get latest pv data
-    pv_systems_sql = get_pv_systems(session=session)
-    pv_yields_sql = get_latest_pv_yield(session=session, pv_systems=pv_systems_sql)
-
-    # remove any data older than 1 hours
-    now_minus_1_hours = datetime.utcnow() - timedelta(hours=1)
-    pv_yields_sql = [
-        pv_yield_sql
-        for pv_yield_sql in pv_yields_sql
-        if pv_yield_sql.datetime_utc >= now_minus_1_hours
-    ]
-
-    # convert to pydantic
-    pv_yields = [PVYield.from_orm(pv_yield_sql) for pv_yield_sql in pv_yields_sql]
-
-    return pv_yields
+# @router.get("/pv_latest", response_model=List[PVYield])
+# def get_latest_pv_data(session: Session = Depends(get_session_pv)) -> List[PVYield]:
+#     """Get Latest PV data from specific pv sites
+#
+#     Only provide PV data received within the last 1 hour
+#     """
+#
+#     # get latest pv data
+#     pv_systems_sql = get_pv_systems(session=session)
+#     pv_yields_sql = get_latest_pv_yield(session=session, pv_systems=pv_systems_sql)
+#
+#     # remove any data older than 1 hours
+#     now_minus_1_hours = datetime.utcnow() - timedelta(hours=1)
+#     pv_yields_sql = [
+#         pv_yield_sql
+#         for pv_yield_sql in pv_yields_sql
+#         if pv_yield_sql.datetime_utc >= now_minus_1_hours
+#     ]
+#
+#     # convert to pydantic
+#     pv_yields = [PVYield.from_orm(pv_yield_sql) for pv_yield_sql in pv_yields_sql]
+#
+#     return pv_yields
