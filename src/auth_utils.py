1325a2117cdf46c5b71dc7da0af3023000ab472a
peterdudfield <peter.dudfield@hotmail.com>
2022-04-20 13:16:49 +0100

    tidy

--- src/auth_utils.py
+++ src/auth_utils.py
@@ -6,18 +6,20 @@ from fastapi_auth0 import Auth0
 domain = os.getenv("AUTH0_DOMAIN", "not-set")
 api_audience = os.getenv("AUTH0_API_AUDIENCE", "not-set")
 
-auth = None
+
+def get_auth():
+    """Make Auth0 object"""
+    return Auth0(domain=domain, api_audience=api_audience)
 
 
 def get_auth_implicit_scheme():
     """Get authentical implicit scheme - this can be mocked in tests"""
-    auth = Auth0(domain=domain, api_audience=api_audience)
+    auth = get_auth()
 
     return auth.implicit_scheme
 
 
 def get_user():
-    """Get user used for authentication
-
-    This function can be mocked for tests"""
+    """Get user used for authentication - this function can be mocked for tests"""
+    auth = get_auth()
     return auth.get_user
