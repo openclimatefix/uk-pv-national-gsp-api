62e1d2a296b98ecc7e09816ba5812e51ba12bca3
peterdudfield <peter.dudfield@hotmail.com>
2022-04-26 14:31:13 +0100

    0.1.28

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from gsp import router as gsp_router
 
 logger = logging.getLogger(__name__)
 
-version = "0.1.27"
+version = "0.1.28"
 description = """
 The Nowcasting API is still under development. It only returns zeros for now.
 """
