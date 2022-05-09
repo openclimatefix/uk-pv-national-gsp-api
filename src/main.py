20a98da50d73f21c806311c7da716b7259060771
BumpVersion Action <bumpversion@github-actions>
2022-05-09 15:19:44 +0000

    Bump version: 0.1.30 → 0.1.31

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from gsp import router as gsp_router
 
 logger = logging.getLogger(__name__)
 
-version = "0.1.30"
+version = "0.1.31"
 description = """
 The Nowcasting API is still under development. It only returns zeros for now.
 """
