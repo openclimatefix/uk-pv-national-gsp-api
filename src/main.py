7047f71f61cc89c2c7ca1444d7d1182370c440a1
BumpVersion Action <bumpversion@github-actions>
2022-05-31 20:06:06 +0000

    Bump version: 0.2.5 → 0.2.6

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ from status import router as status_router
 
 logger = logging.getLogger(__name__)
 
-version = "0.2.5"
+version = "0.2.6"
 description = """
 The Nowcasting API is still under development. It only returns zeros for now.
 """
