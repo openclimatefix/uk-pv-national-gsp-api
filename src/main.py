626877f8d0cfe2f512dbb25507d5063897db9983
BumpVersion Action <bumpversion@github-actions>
2022-09-15 13:44:07 +0000

    Bump version: 0.2.26 → 0.2.27

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-version = "0.2.26"
+version = "0.2.27"
 description = """
 As part of Open Climate Fix’s [open source project](https://github.com/openclimatefix), the
 Nowcasting API is still under development.
