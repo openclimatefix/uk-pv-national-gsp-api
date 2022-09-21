1a0c1bb2e33b333abb870ed064748e47e7ac408f
peterdudfield <peter.dudfield@hotmail.com>
2022-09-21 10:04:52 +0100

    at forecast horizon minutes to national route

--- src/national.py
+++ src/national.py
@@ -1,15 +1,16 @@
 """National API routes"""
 import logging
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from fastapi import APIRouter, Depends
-from nowcasting_datamodel.models import Forecast, GSPYield
+from nowcasting_datamodel.models import Forecast, GSPYield, ForecastValue
 from sqlalchemy.orm.session import Session
 
 from database import (
-    get_forecasts_for_a_specific_gsp_from_database,
     get_session,
     get_truth_values_for_a_specific_gsp_from_database,
+    get_latest_forecast_values_for_a_specific_gsp_from_database,
+    get_forecasts_for_a_specific_gsp_from_database
 )
 
 logger = logging.getLogger(__name__)
@@ -19,35 +20,76 @@ router = APIRouter()
 NationalYield = GSPYield
 
 
-@router.get("/forecast", response_model=Forecast)
-async def get_nationally_aggregated_forecasts(
+@router.get("/forecast", response_model=Union[Forecast, List[ForecastValue]])
+async def get_national_forecast(
     session: Session = Depends(get_session),
     historic: Optional[bool] = False,
-) -> Forecast:
-    """### Returns a national aggregate solar PV energy forecast
-
-    The return object is a forecast object.
-
-    This route aggregrates data from all GSP forecasts and creates an 8-hour solar energy
+    only_forecast_values: Optional[bool] = False,
+    forecast_horizon_minutes: Optional[int] = None,
+) -> Union[Forecast, List[ForecastValue]]:
+    """###     This route aggregrates data from all GSP forecasts and creates an 8-hour solar energy
     generation forecast  in 30-minute interval for all of GB.
 
-    See __Forecast__ and __ForecastValue__ schemas for metadata descriptions.
+    1. Get __recent solar forecast__ for the UK for today and yesterday
+    with system details.
+        - The return object is a solar forecast with forecast details.
+        -The forecast object is returned with expected megawatt generation for the UK
+        for the upcoming 8 hours at every 30-minute interval (targetTime).
+        - Set __only_forecast_values__ ==> FALSE
+        - Setting __historic__ parameter to TRUE returns an object with data
+        from yesterday and today
 
-    params:
-        historic: boolean => TRUE returns yesterday's forecasts in addition to today's forecast
+    2. Get __ONLY__ forecast values for solar national forecast.
+        - Set __only_forecast_values__ to TRUE
+        - Setting a __forecast_horizon_minutes__ parameter retrieves the latest forecast
+        a given set of minutes before the target time.
+        - Return object is a simplified forecast object with __targetTimes__ and
+        __expectedPowerGenerationMegawatts__ at 30-minute intervals.
+        - NB: __historic__ parameter __will not__ work when __only_forecast_values__= TRUE
+
+    Please see the __Forecast__ and __ForecastValue__ schema below for full metadata details.
+
+    #### Parameters
+    - historic: boolean => TRUE returns yesterday's forecasts in addition to today's forecast
+    - only_forecast_values => TRUE returns solar national forecast values
+    - forecast_horizon_minutes: optional forecast horizon in minutes (ex. 35 returns
+    the latest forecast made 35 minutes before the target time)
 
     """
 
     logger.debug("Get national forecasts")
-    national_forecast = get_forecasts_for_a_specific_gsp_from_database(
-        session=session, historic=historic, gsp_id=0
-    )
 
-    logger.debug(
-        f"Got national forecasts with {len(national_forecast.forecast_values)} forecast values"
-    )
+    if not only_forecast_values:
+        logger.debug(f'{"Getting forecast."}')
+        full_forecast = get_forecasts_for_a_specific_gsp_from_database(
+            session=session,
+            gsp_id=0,
+            historic=historic,
+        )
 
-    return national_forecast
+        logger.debug(f"Got forecast.")
+
+        full_forecast.normalize()
+
+        logger.debug(f'{"Normalized forecast."}')
+
+
+        logger.debug(
+            f"Got national forecasts with {len(full_forecast.forecast_values)} forecast values"
+        )
+        return full_forecast
+
+    else:
+
+        national_forecast_values = get_latest_forecast_values_for_a_specific_gsp_from_database(
+            session=session, gsp_id=0, forecast_horizon_minutes=forecast_horizon_minutes
+        )
+
+        logger.debug(
+            f"Got national forecasts with {len(national_forecast_values)} forecast values"
+        )
+
+    return national_forecast_values
 
 
 # corresponds to API route /v0/solar/GB/national/pvlive/, getting PV_Live NationalYield for GB
