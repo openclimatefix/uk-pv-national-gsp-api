916a0fdb41317de413a4a5d428b44db2250adab8
Damien Tanner <damien@tanner.me>
2022-09-21 16:05:46 +0100

    Custom redoc theme
    To be able to set a custom redoc theme, the existing get_redock_html() needed to be extended.
    Right now the route is /newdocs. Once we're happy with the theme, we should change the route to /docs.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from datetime import timedelta
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.openapi.utils import get_openapi
+from starlette.responses import HTMLResponse
 
 from gsp import router as gsp_router
 from national import router as national_router
@@ -19,6 +21,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+title = "Nowcasting API"
 version = "0.2.27"
 description = """
 As part of Open Climate Fix’s [open source project](https://github.com/openclimatefix), the
@@ -60,20 +63,7 @@ You'll find more detailed information for each route in the documentation below.
 If you have any questions, please don't hesitate to get in touch.
 And if you're interested in contributing to our open source project, feel free to join us!
 """
-app = FastAPI(
-    title="Nowcasting API",
-    version=version,
-    description=description,
-    contact={
-        "name": "Open Climate Fix",
-        "url": "https://openclimatefix.org",
-        "email": "info@openclimatefix.org",
-    },
-    license_info={
-        "name": "MIT License",
-        "url": "https://github.com/openclimatefix/nowcasting_api/blob/main/LICENSE",
-    },
-)
+app = FastAPI()
 
 origins = os.getenv("ORIGINS", "https://app.nowcasting.io").split(",")
 app.add_middleware(
@@ -132,3 +122,145 @@ async def get_api_information():
 async def get_favicon() -> FileResponse:
     """Get favicon"""
     return FileResponse("src/favicon.ico")
+
+def get_redoc_html_with_theme(
+    *,
+    openapi_url: str = "./openapi.json",
+    title: str,
+    redoc_js_url: str = "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js",
+    redoc_favicon_url: str = "/favicon.png",
+    with_google_fonts: bool = True,
+    theme = {}
+) -> HTMLResponse:
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>{title}</title>
+    <!-- needed for adaptive design -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    """
+    if with_google_fonts:
+        html += """
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+    """
+    html += f"""
+    <link rel="shortcut icon" href="{redoc_favicon_url}">
+    <!--
+    ReDoc doesn't change outer page styles
+    -->
+    <style>
+      body {{
+        margin: 0;
+        padding: 0;
+      }}
+    </style>
+    </head>
+    <body>
+    <div id="redoc-container"></div>
+    <noscript>
+        ReDoc requires Javascript to function. Please enable it to browse the documentation.
+    </noscript>
+    <script src="{redoc_js_url}"> </script>
+    <script>
+        Redoc.init("{openapi_url}", """ + """{
+            "theme": {
+                "breakpoints": {
+                "small": "10rem",
+                "medium": "40rem",
+                "large": "85rem"
+                },
+                "colors": {
+                "primary": {
+                    "main": "rgba(246, 20, 63, 1)",
+                    "light": "rgba(246, 20, 63, 0.42)"
+                },
+                "success": {
+                    "main": "rgba(28, 184, 65, 1)",
+                    "light": "#81ec9a",
+                    "dark": "#083312",
+                    "contrastText": "#000"
+                },
+                "text": {
+                    "primary": "rgba(0, 0, 0, 1)",
+                    "secondary": "#4d4d4d"
+                },
+                "http": {
+                    "get": "rgba(0, 200, 219, 1)",
+                    "post": "rgba(28, 184, 65, 1)",
+                    "put": "rgba(255, 187, 0, 1)",
+                    "delete": "rgba(254, 39, 35, 1)"
+                }
+                },
+                "typography": {
+                "fontSize": "16px",
+                "fontFamily": "Fira Sans, Roboto, sans-serif",
+                "optimizeSpeed": true,
+                "smoothing": "antialiased",
+                "headings": {
+                    "fontWeight": "bold",
+                    "lineHeight": "1em"
+                },
+                "code": {
+                    "fontWeight": "600",
+                    "color": "rgba(92, 62, 189, 1)",
+                    "wrap": true
+                },
+                "links": {
+                    "color": "rgba(246, 20, 63, 1)",
+                    "visited": "rgba(246, 20, 63, 1)",
+                    "hover": "#fa768f"
+                }
+                },
+                "sidebar": {
+                "width": "300px",
+                "textColor": "#000000"
+                },
+                "rightPanel": {
+                "backgroundColor": "rgba(55, 53, 71, 1)",
+                "textColor": "#ffffff"
+                }
+            }
+        }""" + f""", document.getElementById('redoc-container'))
+    </script>
+    </body>
+    </html>
+    """
+    return HTMLResponse(html)
+
+@app.get("/newdocs", include_in_schema=False)
+async def redoc_html():
+    """### Render ReDoc with custom theme options included"""
+    return get_redoc_html_with_theme(
+        title=title,
+        theme={}
+    ) 
+
+# OpenAPI (ReDoc) custom theme
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=title,
+        version=version,
+        description=description,
+        contact={
+            "name": "Open Climate Fix",
+            "url": "https://openclimatefix.org",
+            "email": "info@openclimatefix.org",
+        },
+        license_info={
+            "name": "MIT License",
+            "url": "https://github.com/openclimatefix/nowcasting_api/blob/main/LICENSE",
+        },
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://www.nowcasting.io/nowcasting.svg"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
