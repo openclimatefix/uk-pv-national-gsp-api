71de6b12d77e3ff70e8ff83a83849accc7ea5b70
peterdudfield <peter.dudfield@hotmail.com>
2023-03-10 13:45:23 +0000

    upgrade caching

--- src/cache.py
+++ src/cache.py
@@ -1,7 +1,11 @@
 import os
+import logging
+import json
 from functools import wraps
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
+
+logger = logging.getLogger(__name__)
 
 CACHE_TIME_SECONDS = 120
 cache_time_seconds = int(os.getenv('CACHE_TIME_SECONDS', CACHE_TIME_SECONDS))
@@ -21,26 +25,33 @@ def cache_response(func):
             return {"message": "Hello World"}
     ```
     """
-    response = None
-    last_updated = None
+    response = {}
+    last_updated = {}
 
     @wraps(func)
     async def wrapper(*args, **kwargs):
         nonlocal response
         nonlocal last_updated
 
-        print(response)
-        if not last_updated:
-            last_updated = datetime.now()
-            response = await func(*args, **kwargs)
-            return response
+        route_variables = kwargs.copy()
+        route_variables.pop('session')
+        route_variables.pop('user')
+        route_variables = json.dumps(route_variables)
 
-        now = datetime.now()
-        if now - timedelta(seconds=cache_time_seconds) > last_updated:
-            last_updated = datetime.now()
-            response = await func(*args, **kwargs)
-            return response
+        if route_variables not in last_updated:
+            logger.debug('First time this is route run')
+            last_updated[route_variables] = datetime.now(tz=timezone.utc)
+            response[route_variables] = await func(*args, **kwargs)
+            return response[route_variables]
 
-        return response
+        now = datetime.now(tz=timezone.utc)
+        if now - timedelta(seconds=cache_time_seconds) > last_updated[route_variables]:
+            logger.debug(f'not using cache as longer than {cache_time_seconds} seconds')
+            last_updated[route_variables] = now
+            response[route_variables] = await func(*args, **kwargs)
+            return response[route_variables]
+
+        logger.debug('Using cache route')
+        return response[route_variables]
 
     return wrapper
