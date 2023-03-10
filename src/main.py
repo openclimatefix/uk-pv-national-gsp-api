eb79076ab603f50db275bc827bbc84f02061777a
BumpVersion Action <bumpversion@github-actions>
2023-03-10 10:22:07 +0000

    Bump version: 0.2.54 → 1.0.0

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 folder = os.path.dirname(os.path.abspath(__file__))
 
 title = "Nowcasting API"
-version = "0.2.54"
+version = "1.0.0"
 
 description = """
 As part of Open Climate Fix’s [open source project](https://github.com/openclimatefix), the
