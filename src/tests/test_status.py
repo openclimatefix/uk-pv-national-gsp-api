94f97c29ceaeb5bec63563cd7f21360f4cd692b8
peterdudfield <peter.dudfield@hotmail.com>
2023-03-13 12:09:30 +0000

    add a status check for see if the forecast has run in the last 2 hours

--- src/tests/test_status.py
+++ src/tests/test_status.py
@@ -1,6 +1,9 @@
 """ Test for main app """
+from datetime import datetime, timedelta, timezone
 from fastapi.testclient import TestClient
-from nowcasting_datamodel.models import Status
+from nowcasting_datamodel.models import Status, ForecastSQL
+
+from freezegun import freeze_time
 
 from database import get_session
 from main import app
@@ -21,3 +24,38 @@ def test_read_latest_status(db_session):
     returned_status = Status(**response.json())
     assert returned_status.message == status.message
     assert returned_status.status == status.status
+
+
+@freeze_time('2023-01-01')
+def test_check_last_forecast_run_no_forecast(db_session):
+    """Check main check_last_forecast_run fales where there are not forecasts"""
+    app.dependency_overrides[get_session] = lambda: db_session
+
+    response = client.get("/v0/solar/GB/check_last_forecast_run")
+    assert response.status_code == 404
+
+
+@freeze_time('2023-01-02')
+def test_check_last_forecast_run_correct(db_session):
+    """Check check_last_forecast_run works fine"""
+    forecast_creation_time = datetime.now(tz=timezone.utc) - timedelta(minutes=5)
+    forecast = ForecastSQL(forecast_creation_time=forecast_creation_time)
+    db_session.add(forecast)
+
+    app.dependency_overrides[get_session] = lambda: db_session
+
+    response = client.get("/v0/solar/GB/check_last_forecast_run")
+    assert response.status_code == 200
+
+
+@freeze_time('2023-01-03')
+def test_check_last_forecast_error(db_session):
+    """Check check_last_forecast_run works fine"""
+    forecast_creation_time = datetime.now(tz=timezone.utc) - timedelta(hours=3)
+    forecast = ForecastSQL(forecast_creation_time=forecast_creation_time)
+    db_session.add(forecast)
+
+    app.dependency_overrides[get_session] = lambda: db_session
+
+    response = client.get("/v0/solar/GB/check_last_forecast_run")
+    assert response.status_code == 404
