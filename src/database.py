65c634e62672fcb71b61b9df5abcb7c96d5cf91a
peterdudfield <peter.dudfield@hotmail.com>
2023-04-06 15:02:18 +0100

    fix

--- src/database.py
+++ src/database.py
@@ -122,7 +122,7 @@ def get_latest_forecast_values_for_a_specific_gsp_from_database(
     start_datetime = get_start_datetime()
 
     if forecast_horizon_minutes is None:
-        if gsp_id is not 0:
+        if gsp_id != 0:
             forecast_values = get_forecast_values_latest(
                 session=session, gsp_id=gsp_id, start_datetime=start_datetime
             )
