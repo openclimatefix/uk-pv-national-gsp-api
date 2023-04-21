c8f07ab6e3f42b058ee979819ff05a8721f0c2ff
BumpVersion Action <bumpversion@github-actions>
2023-04-21 11:22:41 +0000

    Bump version: 1.4.0 → 1.4.1

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ logger = structlog.stdlib.get_logger()
 folder = os.path.dirname(os.path.abspath(__file__))
 
 title = "Nowcasting API"
-version = "1.4.0"
+version = "1.4.1"
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
