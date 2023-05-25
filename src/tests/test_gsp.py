e5f558112528dab290ca389b3f3ab2ad7933d741
peterdudfield <peter.dudfield@hotmail.com>
2023-05-25 16:45:25 +0100

    sort imports

--- src/tests/test_gsp.py
+++ src/tests/test_gsp.py
@@ -2,7 +2,6 @@
 from datetime import datetime, timezone
 
 from freezegun import freeze_time
-from nowcasting_datamodel.read.read import get_model
 from nowcasting_datamodel.fake import make_fake_forecasts
 from nowcasting_datamodel.models import (
     Forecast,
@@ -12,6 +11,7 @@ from nowcasting_datamodel.models import (
     LocationWithGSPYields,
     ManyForecasts,
 )
+from nowcasting_datamodel.read.read import get_model
 from nowcasting_datamodel.save.update import update_all_forecast_latest
 
 from database import get_session
