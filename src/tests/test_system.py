82b7dc2d01e53b30aeae9b440de0cdd539360297
peterdudfield <peter.dudfield@hotmail.com>
2023-06-20 13:10:00 +0100

    add test, to check capacites are floats

--- src/tests/test_system.py
+++ src/tests/test_system.py
@@ -2,6 +2,7 @@
 
 from nowcasting_datamodel.fake import make_fake_forecasts
 from nowcasting_datamodel.models import Location
+from nowcasting_datamodel.read.read import get_location
 
 from database import get_session
 from main import app
@@ -14,6 +15,13 @@ def test_get_gsp_systems(db_session, api_client):
     db_session.add_all(forecasts)
     db_session.commit()
 
+    # update to installed capacity to a float
+    location = get_location(
+        gsp_id=1, session=db_session
+    )
+    location.installed_capacity_mw = 1.1
+    db_session.commit()
+
     app.dependency_overrides[get_session] = lambda: db_session
 
     response = api_client.get("v0/system/GB/gsp/")
@@ -21,6 +29,7 @@ def test_get_gsp_systems(db_session, api_client):
 
     locations = [Location(**location) for location in response.json()]
     assert len(locations) == 10
+    assert locations[1].installed_capacity_mw == 1.1
 
 
 def test_gsp_boundaries(db_session, api_client):
