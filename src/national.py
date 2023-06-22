690325df4115666a26c47ab51e5f194dcb5ef0d0
rachel-labri-tipton <rachemtipton@gmail.com>
2023-06-22 17:33:12 +0200

    updates national routes

--- src/national.py
+++ src/national.py
@@ -46,10 +46,12 @@ def get_national_forecast(
     This route aggregrates data from all GSP forecasts in Great Britain,
     creating a national, 8-hour,
     solar generation forecast in 30-minute intervals.
+    The _forecast_horizon_minutes_ parameter allows
+    a user to query for a forecast closer to the target time.
 
     #### Parameters
-    - **forecast_horizon_minutes**: optional forecast horizon in minutes (ex. 60
-    returns the latest forecast made 60 minutes before the target time)
+    - **forecast_horizon_minutes**: optional forecast horizon in minutes (ex.
+    60 returns the latest forecast made 60 minutes before the target time)
 
     """
 
@@ -65,7 +67,9 @@ def get_national_forecast(
             historic=historic,
         )
 
-        logger.debug(f"Got forecast Now adjusting by at most {adjust_limit} MW and normalizing.")
+        logger.debug(
+            f"Got forecast Now adjusting by at most {adjust_limit} MW and normalizing."
+        )
 
         full_forecast.adjust(limit=adjust_limit)
         full_forecast.normalize()
@@ -79,10 +83,12 @@ def get_national_forecast(
         return full_forecast
 
     else:
-        national_forecast_values = get_latest_forecast_values_for_a_specific_gsp_from_database(
-            session=session,
-            gsp_id=0,
-            forecast_horizon_minutes=forecast_horizon_minutes,
+        national_forecast_values = (
+            get_latest_forecast_values_for_a_specific_gsp_from_database(
+                session=session,
+                gsp_id=0,
+                forecast_horizon_minutes=forecast_horizon_minutes,
+            )
         )
 
         logger.debug(
@@ -90,7 +96,9 @@ def get_national_forecast(
             f"Now adjusting by at most {adjust_limit} MW"
         )
 
-    national_forecast_values = [f.adjust(limit=adjust_limit) for f in national_forecast_values]
+    national_forecast_values = [
+        f.adjust(limit=adjust_limit) for f in national_forecast_values
+    ]
 
     return national_forecast_values
 
@@ -108,15 +116,25 @@ def get_national_pvlive(
     session: Session = Depends(get_session),
     user: Auth0User = Security(get_user()),
 ) -> List[NationalYield]:
-    """### Get national PV_Live values for yesterday and today
+    """### Get national PV_Live values for yesterday and/or today
 
-    Get a series of real-time solar energy generation readings from PV_Live.
+    Returns a series of real-time solar energy generation readings from
+    PV_Live for all of Great Britain.
+    _In-day_ values are PV generation estimates for the current day,
+    while _day-after_ values are
+    updated PV generation truths for the previous day along with
+    _in-day_ estimates for the current day.
+
+    If nothing is set for the _regime_ parameter, the route will return
+    _in-day_ values for the current day.
 
     #### Parameters
     - regime: can choose __in-day__ or __day-after__
     """
 
-    logger.info(f"Get national PV Live estimates values " f"for regime {regime} for {user}")
+    logger.info(
+        f"Get national PV Live estimates values " f"for regime {regime} for {user}"
+    )
 
     save_api_call_to_db(session=session, user=user, request=request)
 
