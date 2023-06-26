cd149fcf5fb72b5a8a71c46a730b483394c1c694
rachel-labri-tipton <rachemtipton@gmail.com>
2023-06-26 10:15:54 +0200

    remove tests for historic national forecast

--- src/tests/test_national.py
+++ src/tests/test_national.py
@@ -27,44 +27,6 @@ def test_read_latest_national(db_session, api_client):
     _ = Forecast(**response.json())
 
 
-def test_read_latest_national_historic_forecast_value(db_session, api_client):
-    """Check main solar/GB/national/forecast route works"""
-
-    model = get_model(db_session, name="cnn", version="0.0.1")
-
-    forecast = make_fake_national_forecast(
-        session=db_session, t0_datetime_utc=datetime.now(tz=timezone.utc)
-    )
-    forecast.model = model
-
-    db_session.add(forecast)
-    update_all_forecast_latest(forecasts=[forecast], session=db_session, model_name="cnn")
-
-    app.dependency_overrides[get_session] = lambda: db_session
-
-    response = api_client.get("/v0/solar/GB/national/forecast/?only_forecast_values=True")
-    assert response.status_code == 200
-
-    _ = [ForecastValue(**f) for f in response.json()]
-
-
-def test_read_latest_national_historic(db_session, api_client):
-    """Check main solar/GB/national/forecast route works"""
-
-    forecast = make_fake_national_forecast(
-        session=db_session, t0_datetime_utc=datetime.now(tz=timezone.utc)
-    )
-    db_session.add(forecast)
-    update_all_forecast_latest(forecasts=[forecast], session=db_session)
-
-    app.dependency_overrides[get_session] = lambda: db_session
-
-    response = api_client.get("/v0/solar/GB/national/forecast/?historic=True")
-    assert response.status_code == 200
-
-    _ = Forecast(**response.json())
-
-
 @freeze_time("2022-01-01")
 def test_read_truth_national_gsp(db_session, api_client):
     """Check main solar/GB/national/pvlive route works"""
