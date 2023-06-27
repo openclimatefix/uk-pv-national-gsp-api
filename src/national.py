14ad9f2dcc1fb23b22b3ff8ba059a0febfe42dbc
rachel-labri-tipton <rachemtipton@gmail.com>
2023-06-27 18:21:47 +0200

    updates get national forecast description

--- src/national.py
+++ src/national.py
@@ -40,11 +40,11 @@ def get_national_forecast(
 ) -> Union[Forecast, List[ForecastValue]]:
     """Get the National Forecast
 
-    This route aggregrates data from all GSP forecasts in Great Britain,
-    creating a national, 8-hour,
-    solar generation forecast in 30-minute intervals.
+    This route returns the most recent forecast for each _target_time_.
+
     The _forecast_horizon_minutes_ parameter allows
-    a user to query for a forecast closer than 8 hours to the target time.
+    a user to query for a forecast that is made this number, or horizon, of
+    minutes before the _target_time_.
 
     For example, if the target time is 10am today, the forecast made at 2am
     today is the 8-hour forecast for 10am, and the forecast made at 6am for
@@ -64,15 +64,19 @@ def get_national_forecast(
     save_api_call_to_db(session=session, user=user, request=request)
 
     logger.debug("Getting forecast.")
-    national_forecast_values = get_latest_forecast_values_for_a_specific_gsp_from_database(
-        session=session, gsp_id=0, forecast_horizon_minutes=forecast_horizon_minutes
+    national_forecast_values = (
+        get_latest_forecast_values_for_a_specific_gsp_from_database(
+            session=session, gsp_id=0, forecast_horizon_minutes=forecast_horizon_minutes
+        )
     )
 
     logger.debug(
         f"Got national forecasts with {len(national_forecast_values)} forecast values. "
         f"Now adjusting by at most {adjust_limit} MW"
     )
-    national_forecast_values = [f.adjust(limit=adjust_limit) for f in national_forecast_values]
+    national_forecast_values = [
+        f.adjust(limit=adjust_limit) for f in national_forecast_values
+    ]
 
     return national_forecast_values
 
@@ -107,7 +111,9 @@ def get_national_pvlive(
     - **regime**: can choose __in-day__ or __day-after__
     """
 
-    logger.info(f"Get national PV Live estimates values " f"for regime {regime} for  {user}")
+    logger.info(
+        f"Get national PV Live estimates values " f"for regime {regime} for  {user}"
+    )
 
     save_api_call_to_db(session=session, user=user, request=request)
 
