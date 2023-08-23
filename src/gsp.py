fa17b8d3ec2bb74cfb5a231f6251c429a0f7d60f
peterdudfield <peter.dudfield@hotmail.com>
2023-08-23 18:43:01 +0100

    isort

--- src/gsp.py
+++ src/gsp.py
@@ -6,7 +6,6 @@ from fastapi import APIRouter, Depends, Request, Security, status
 from fastapi.responses import Response
 from fastapi_auth0 import Auth0User
 from nowcasting_datamodel.models import Forecast, ForecastValue, ManyForecasts
-
 from sqlalchemy.orm.session import Session
 
 from auth_utils import get_auth_implicit_scheme, get_user
