1d6a52415274d7fd026b258ebe32922459d5d559
peterdudfield <peter.dudfield@hotmail.com>
2023-08-28 21:30:16 +0100

    remove blending, and just load blend

--- src/database.py
+++ src/database.py
@@ -22,6 +22,7 @@ from nowcasting_datamodel.read.blend.blend import get_blend_forecast_values_late
 from nowcasting_datamodel.read.read import (
     get_all_gsp_ids_latest_forecast,
     get_all_locations,
+    get_forecast_values,
     get_forecast_values_latest,
     get_latest_forecast,
     get_latest_national_forecast,
@@ -176,13 +177,12 @@ def get_latest_forecast_values_for_a_specific_gsp_from_database(
         )
 
     else:
-        forecast_values = get_blend_forecast_values_latest(
+        forecast_values = get_forecast_values(
             session=session,
             gsp_id=gsp_id,
             start_datetime=start_datetime,
             forecast_horizon_minutes=forecast_horizon_minutes,
-            weights=weights,
-            model_names=["cnn", "National_xg", "pvnet_v2"],
+            model_name="blend",
         )
 
     # convert to pydantic objects
