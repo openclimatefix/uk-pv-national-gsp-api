7883cf780effa44acdf64ffd216b663329fe3abf
peterdudfield <peter.dudfield@hotmail.com>
2023-09-27 16:14:15 +0100

    fix

--- src/national.py
+++ src/national.py
@@ -78,19 +78,28 @@ def get_national_forecast(
                 detail="Can not set forecast_horizon_minutes when including metadata",
             )
 
+        if creation_limit_utc is None:
+            historic = True
+        else:
+            historic = False
+
         forecast = get_latest_forecast_for_gsps(
             session=session,
             gsp_ids=[0],
             model_name="blend",
-            historic=True,
+            historic=historic,
             preload_children=True,
             start_target_time=start_datetime_utc,
             end_target_time=end_datetime_utc,
-            start_created_utc=creation_limit_utc,
+            end_created_utc=creation_limit_utc,
         )
         forecast = forecast[0]
 
-        forecast = NationalForecast.from_orm_latest(forecast)
+        if historic:
+            forecast = NationalForecast.from_orm_latest(forecast)
+        else:
+            forecast = NationalForecast.from_orm(forecast)
+
         forecasts = filter_forecast_values(
             forecasts=[forecast],
             start_datetime_utc=start_datetime_utc,
