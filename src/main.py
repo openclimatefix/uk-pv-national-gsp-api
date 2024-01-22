ff228fa7e93aae49b5d4686098eefdb1647979ae
BumpVersion Action <bumpversion@github-actions>
2024-01-22 17:09:50 +0000

    Bump version: 1.5.38 → 1.5.39 [skip ci]

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ logger = structlog.stdlib.get_logger()
 folder = os.path.dirname(os.path.abspath(__file__))
 
 title = "Quartz Solar API"
-version = "1.5.38"
+version = "1.5.39"
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
