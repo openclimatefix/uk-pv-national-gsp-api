3aa4f6abc80962c871a12d6e5ab9314717c5c087
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2024-01-22 17:09:26 +0000

    Remove cached responses older than 4 mins (#318)
    * remove old cache
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fastapi==0.108.0
    
    * pathy=0.10.3
    
    * pathy==0.10.3
    
    * update requirements
    
    * remove background tasks in cache
    
    * role back requirements
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/cache.py
+++ src/cache.py
@@ -13,6 +13,33 @@ logger = structlog.stdlib.get_logger()
 
 CACHE_TIME_SECONDS = 120
 cache_time_seconds = int(os.getenv("CACHE_TIME_SECONDS", CACHE_TIME_SECONDS))
+DELETE_CACHE_TIME_SECONDS = 240
+delete_cache_time_seconds = int(os.getenv("DELETE_CACHE_TIME_SECONDS", DELETE_CACHE_TIME_SECONDS))
+
+
+def remove_old_cache(
+    last_updated: dict, response: dict, remove_cache_time_seconds: float = delete_cache_time_seconds
+):
+    """
+    Remove old cache entries from the cache
+
+    :param last_updated: dict of last updated times
+    :param response: dict of responses, same keys as last_updated
+    :param remove_cache_time_seconds: the amount of time, after which the cache should be removed
+    """
+    now = datetime.now(tz=timezone.utc)
+    logger.info("Removing old cache entries")
+    keys_to_remove = []
+    for key, value in last_updated.items():
+        if now - timedelta(seconds=remove_cache_time_seconds) > value:
+            logger.debug(f"Removing {key} from cache, ({value})")
+            keys_to_remove.append(key)
+
+    for key in keys_to_remove:
+        last_updated.pop(key)
+        response.pop(key)
+
+    return last_updated, response
 
 
 def cache_response(func):
@@ -51,6 +78,8 @@ def cache_response(func):
         for var in ["session", "user", "request"]:
             if var in route_variables:
                 route_variables.pop(var)
+
+        last_updated, response = remove_old_cache(last_updated, response)
 
         # make route_variables into a string
         route_variables = json.dumps(route_variables)
