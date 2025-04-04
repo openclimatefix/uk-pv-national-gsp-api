3b2e3be5b9229a61aadf81d516a969a095ba7ceb
Yuvraaj Narula <ynarula678@gmail.com>
2025-04-04 23:25:09 +0530

    Async tests and cache key generation

--- nowcasting_api/tests/test_gsp.py
+++ nowcasting_api/tests/test_gsp.py
@@ -144,16 +144,16 @@ def test_read_latest_all_gsp_filter_gsp(db_session, api_client):
     assert len(r.forecasts[0].forecast_values) == 16
 
 
-def test_read_latest_gsp_id_greater_than_total(db_session, api_client):
+async def test_read_latest_gsp_id_greater_than_total(db_session, api_client):
     """Check that request with gsp_id>=318 returns 204"""
 
     gsp_id = 318
-    response = api_client.get(f"/v0/solar/GB/gsp/forecast/{gsp_id}/?historic=False&normalize=True")
+    response = await api_client.get(f"/v0/solar/GB/gsp/forecast/{gsp_id}/?historic=False&normalize=True")
 
     assert response.status_code == 204
 
 
-def test_read_latest_gsp_id_equal_to_total(db_session, api_client):
+async def test_read_latest_gsp_id_equal_to_total(db_session, api_client):
     """Check that request with gsp_id<318 returns 200"""
 
     _ = make_fake_forecasts(gsp_ids=[317], session=db_session, add_latest=True, model_name="blend")
@@ -161,7 +161,7 @@ def test_read_latest_gsp_id_equal_to_total(db_session, api_client):
 
     app.dependency_overrides[get_session] = lambda: db_session
 
-    response = api_client.get("/v0/solar/GB/gsp/forecast/317")
+    response = await api_client.get("/v0/solar/GB/gsp/forecast/317")
 
     assert response.status_code == 200
 
@@ -251,7 +251,7 @@ def test_read_latest_all_gsp_historic_compact(db_session, api_client):
 
 
 @freeze_time("2022-01-01")
-def test_read_truths_for_a_specific_gsp(db_session, api_client):
+async def test_read_truths_for_a_specific_gsp(db_session, api_client):
     """Check main solar/GB/gsp/pvlive route works"""
 
     gsp_yield_1 = GSPYield(datetime_utc=datetime(2022, 1, 2), solar_generation_kw=1)
@@ -278,7 +278,7 @@ def test_read_truths_for_a_specific_gsp(db_session, api_client):
 
     app.dependency_overrides[get_session] = lambda: db_session
 
-    response = api_client.get("/v0/solar/GB/gsp/pvlive/122")
+    response = await api_client.get("/v0/solar/GB/gsp/pvlive/122")
 
     assert response.status_code == 200
 
@@ -288,17 +288,17 @@ def test_read_truths_for_a_specific_gsp(db_session, api_client):
     _ = [GSPYield(**gsp_yield) for gsp_yield in r_json]
 
 
-def test_read_pvlive_for_gsp_id_over_total(db_session, api_client):
+async def test_read_pvlive_for_gsp_id_over_total(db_session, api_client):
     """Check solar/GB/gsp/pvlive returns 204 when gsp_id over total"""
 
     gsp_id = 318
-    response = api_client.get(f"/v0/solar/GB/gsp/pvlive/{gsp_id}")
+    response = await api_client.get(f"/v0/solar/GB/gsp/pvlive/{gsp_id}")
 
     assert response.status_code == 204
 
 
 @freeze_time("2022-01-01")
-def test_read_truths_for_gsp_id_less_than_total(db_session, api_client):
+async def test_read_truths_for_gsp_id_less_than_total(db_session, api_client):
     """Check solar/GB/gsp/pvlive returns 200 when gsp_id under total"""
 
     gsp_yield = GSPYield(datetime_utc=datetime(2022, 1, 2), solar_generation_kw=1)
@@ -318,7 +318,7 @@ def test_read_truths_for_gsp_id_less_than_total(db_session, api_client):
 
     app.dependency_overrides[get_session] = lambda: db_session
 
-    response = api_client.get(f"/v0/solar/GB/gsp/pvlive/{gsp_id}")
+    response = await api_client.get(f"/v0/solar/GB/gsp/pvlive/{gsp_id}")
 
     assert response.status_code == 200
 
