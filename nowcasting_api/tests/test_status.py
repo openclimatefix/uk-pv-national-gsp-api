8b15620857dbd30c74aceb8de034459f17f79c64
Yuvraaj Narula <ynarula678@gmail.com>
2025-04-22 23:46:17 +0530

    lint

--- nowcasting_api/tests/test_status.py
+++ nowcasting_api/tests/test_status.py
@@ -5,7 +5,6 @@ import tempfile
 from datetime import datetime, timedelta, timezone
 
 import fsspec
-import pytest
 from fastapi.testclient import TestClient
 from freezegun import freeze_time
 from nowcasting_datamodel.models import (
