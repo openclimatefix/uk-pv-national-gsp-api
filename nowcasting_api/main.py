21eeace89cbd043bfe3f71d4d36c112c6a950522
BumpVersion Action <bumpversion@github-actions>
2025-07-04 12:37:29 +0000

    Bump version: 1.5.106 → 1.6.0 [skip ci]

--- nowcasting_api/main.py
+++ nowcasting_api/main.py
@@ -48,7 +48,7 @@ logger = structlog.stdlib.get_logger()
 folder = os.path.dirname(os.path.abspath(__file__))
 
 title = "Quartz Solar API"
-version = "1.5.106"
+version = "1.6.0"
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
