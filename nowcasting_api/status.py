6567bd3f447a4a09d93d2f90e08dd04c0e4a9247
peterdudfield <peter.dudfield@hotmail.com>
2025-07-09 11:03:03 +0100

    tidy

--- nowcasting_api/status.py
+++ nowcasting_api/status.py
@@ -29,7 +29,7 @@ def get_status(request: Request, session: Session = Depends(get_session)) -> Sta
     route is where the OCF team communicates the forecast status to users.
 
     """
-    save_api_call_to_db(session=session, user=None, request=request)
+    save_api_call_to_db(session=session, request=request)
 
     logger.debug("Get status")
     return get_latest_status_from_database(session=session)
