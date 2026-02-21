f710d5c36c045f6150cdc7b1d843bfd63178a08f
Brad Fulford <41056982+braddf@users.noreply.github.com>
2026-02-21 17:59:06 +0000

    fix[cache]: replace double dict cache logic (#532)
    * fix[cache]: replace double dict cache logic
    
    Took 1 hour 52 minutes
    
    * fix[cache]: avoid immediately using result from cache dictionary
    
    Took 4 minutes
    
    * increase cache size to 1024 ( there are ~340 gsps)
    
    ---------
    
    Co-authored-by: Peter Dudfield <peter.dudfield@hotmail.com>

--- nowcasting_api/cache.py
+++ nowcasting_api/cache.py
@@ -3,56 +3,19 @@
 import json
 import os
 import time
-from datetime import datetime, timedelta, timezone
 from functools import wraps
 
 import structlog
 from apitally.fastapi import set_consumer
+from cachetools import TTLCache
 from database import save_api_call_to_db
 
 logger = structlog.stdlib.get_logger()
 
 CACHE_TIME_SECONDS = 120
 cache_time_seconds = int(os.getenv("CACHE_TIME_SECONDS", CACHE_TIME_SECONDS))
-DELETE_CACHE_TIME_SECONDS = 240
-delete_cache_time_seconds = int(os.getenv("DELETE_CACHE_TIME_SECONDS", DELETE_CACHE_TIME_SECONDS))
 
 QUERY_WAIT_SECONDS = int(os.getenv("QUERY_WAIT_SECONDS", 30))
-
-
-def remove_old_cache(
-    last_updated: dict, response: dict, remove_cache_time_seconds: float = delete_cache_time_seconds
-):
-    """
-    Remove old cache entries from the cache
-
-    :param last_updated: dict of last updated times
-    :param response: dict of responses, same keys as last_updated
-    :param remove_cache_time_seconds: the amount of time, after which the cache should be removed
-    """
-    now = datetime.now(tz=timezone.utc)
-    logger.info("Checking and removing old cache entries")
-    keys_to_remove = []
-    last_updated_copy = last_updated.copy()
-    for key, value in last_updated_copy.items():
-        if now - timedelta(seconds=remove_cache_time_seconds) > value:
-            logger.debug(f"Removing {key} from cache, ({value})")
-            keys_to_remove.append(key)
-
-    del last_updated_copy
-    logger.debug(f"Removing {len(keys_to_remove)} keys from cache")
-
-    for key in keys_to_remove:
-        try:
-            last_updated.pop(key)
-            response.pop(key)
-        except KeyError:
-            logger.warning(
-                f"Could not remove {key} from cache. "
-                f"This could be because it has already been removed"
-            )
-
-    return last_updated, response
 
 
 def cache_response(func):
@@ -69,14 +32,12 @@ def cache_response(func):
             return {"message": "Hello World"}
     ```
     """
-    response = {}
-    last_updated = {}
+    cache = TTLCache(maxsize=1024, ttl=cache_time_seconds)
     currently_running = {}
 
     @wraps(func)
     def wrapper(*args, **kwargs):  # noqa
-        nonlocal response
-        nonlocal last_updated
+        nonlocal cache
         nonlocal currently_running
 
         # get the variables that go into the route
@@ -101,23 +62,30 @@ def cache_response(func):
             if var in route_variables:
                 route_variables.pop(var)
 
-        last_updated, response = remove_old_cache(last_updated, response)
-
         # make route_variables into a string\
         # TODO add url
         # TODO sort route variables alphabetically
         # url = request.url
         route_variables = json.dumps(route_variables)
 
+        cache.expire()
+        logger.info(
+            "Cache stats",
+            cache_size=len(cache),
+            cache_maxsize=cache.maxsize,
+            currently_running_keys=len(currently_running),
+            cache_memory=cache.currsize,
+        )
+
         # use case
         # A. First time we call this the route -> call the route (1.1)
         # B. Second time we call the route, but its running at the moment.
         #   Wait for it to finish. (1.0)
-        # C. The cached result it old, and its not running, --> call the route (1.2)
+        # C. [removed] The cached result it old, and its not running, --> call the route (1.2)
         # D. The cached result is empty, and its running, --> (1.0)
         # E. The cached result is up to date, --> use the cache (1.4)
         # F. It is current being run, wait a bit, then try to use those results (1.0)
-        # G. If the cache results is None, lets wait a few seconds,
+        # G. [removed] If the cache results is None, lets wait a few seconds,
         #   then try to use the cache (1.3)
 
         # 1.0
@@ -132,15 +100,15 @@ def cache_response(func):
                     logger.debug(
                         f"route finished after {attempt} seconds, returning cached response"
                     )
-                    if route_variables in response:
-                        return response[route_variables]
+                    if route_variables in cache:
+                        return cache[route_variables]
                     else:
                         logger.warning(
                             "Process finished running but response not "
                             "in cache. Setting this route as not running, "
                             "and continuing"
                         )
-                        currently_running[route_variables] = False
+                        currently_running.pop(route_variables, None)
                         break
 
             logger.warning(
@@ -148,69 +116,27 @@ def cache_response(func):
                 f"in cache. Setting this route as not running, "
                 f"and continuing"
             )
-            currently_running[route_variables] = False
+            currently_running.pop(route_variables, None)
 
         # 1.1 check if its been called before and not currently running
-        if (route_variables not in last_updated) and (
-            not currently_running.get(route_variables, False)
-        ):
+        if (route_variables not in cache) and (not currently_running.get(route_variables, False)):
             logger.debug("1.1 First time this is route run, and not running now")
 
             # run the route
             currently_running[route_variables] = True
-            response[route_variables] = func(*args, **kwargs)
-            currently_running[route_variables] = False
-            last_updated[route_variables] = datetime.now(tz=timezone.utc)
+            result = func(*args, **kwargs)
+            cache[route_variables] = result
+            currently_running.pop(route_variables, None)
 
-            return response[route_variables]
+            return result
 
-        # 1.2 rerun if cache time out is up and not currently running
-        now = datetime.now(tz=timezone.utc)
-        if route_variables not in last_updated:
-            pass
-        elif now - timedelta(seconds=cache_time_seconds) > last_updated.get(route_variables) and (
-            not currently_running.get(route_variables, False)
-        ):
-            logger.debug(
-                f"Not using cache as longer than {cache_time_seconds} seconds, and not running now"
-            )
+        # 1.2 [removed cache staleness check as is now covered by TTLCache expiry]
 
-            # run the route
-            currently_running[route_variables] = True
-            response[route_variables] = func(*args, **kwargs)
-            currently_running[route_variables] = False
-            last_updated[route_variables] = now
-
-            return response[route_variables]
-
-        # 1.3. re-run if response is not cached for some reason or is empty
-        if route_variables not in response or response[route_variables] is None:
-            logger.debug("1.3 not using cache as response is empty")
-            attempt = 0
-            # wait until response has been cached
-            while attempt < QUERY_WAIT_SECONDS:
-                logger.debug(f"waiting for response to be cached, {attempt} seconds elapsed")
-                time.sleep(1)
-                attempt += 1
-                if route_variables in response and response[route_variables] is not None:
-                    logger.debug(
-                        f"response cached after {attempt} seconds, returning cached response"
-                    )
-                    break
-            if attempt >= QUERY_WAIT_SECONDS:
-                # if response is not in cache after QUERY_WAIT_SECONDS seconds, re-run
-                logger.debug(f"response not cached after {QUERY_WAIT_SECONDS} seconds, re-running")
-
-                # run the route
-                currently_running[route_variables] = True
-                response[route_variables] = func(*args, **kwargs)
-                currently_running[route_variables] = False
-                last_updated[route_variables] = now
-
-                return response[route_variables]
+        # 1.3 [removed because this logical route was due to last_updated + response dicts mismatch
+        #      that is now handled by TTLCache internally]
 
         # 1.4 use cache
-        logger.debug(f"Using cache route, cache made at {last_updated[route_variables]}")
-        return response[route_variables]
+        logger.debug("Using cached response")
+        return cache[route_variables]
 
     return wrapper
